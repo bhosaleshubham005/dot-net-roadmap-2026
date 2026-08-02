b56fe947ae2ede2b69c03906be29572fc5b79449
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-08-02 12:55:59 +0530

    feat: add HelloWorld and StudentProfile console applications

---
+++ 02-CSharp-Basics/StudentProfile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=================================");
+        Console.WriteLine("       Student Profile");
+        Console.WriteLine("=================================");
+
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine()!;
+
+        Console.Write("Enter Age: ");
+        int age = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Enter City: ");
+        string city = Console.ReadLine()!;
+
+        Console.Write("Enter Favourite Programming Language: ");
+        string language = Console.ReadLine()!;
+
+        Console.WriteLine();
+
+        Console.WriteLine("profile created successfully!");
+        Console.WriteLine($"Name      : {name}");
+        Console.WriteLine($"Age       : {age}");
+        Console.WriteLine($"City      : {city}");
+        Console.WriteLine($"Language  : {language}");
+        Console.WriteLine("");
+
+
+    }
+}
