fa022ccd1aa525f3b88ce957405de032ee3b1e6a
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-08-04 11:42:04 +0530

    feat: implement ATM system using if-else and nested if

---
+++ 02-CSharp-Basics/ATMsystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int correctPin = 1234;
+        double balance = 50000;
+
+        Console.WriteLine("===== ATM Machine =====");
+
+        Console.Write("Enter PIN: ");
+        int pin = int.Parse(Console.ReadLine()!);
+
+        if (pin == correctPin)
+        {
+            Console.Write("Enter Withdrawal Amount: ");
+            double amount = double.Parse(Console.ReadLine()!);
+
+            if (amount <= balance)
+            {
+                balance -= amount;
+
+                Console.WriteLine();
+                Console.WriteLine("Transaction Successful!");
+                Console.WriteLine($"Remaining Balance : {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid PIN.");
+        }
+
+        Console.ReadKey();
+    }
+}
