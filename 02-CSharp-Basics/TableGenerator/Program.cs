af360edfc7f30c79f7968c71b6856b46881eec58
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-08-04 13:00:29 +0530

    table generator mini project developed

---
+++ 02-CSharp-Basics/TableGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"\nMultiplication Table of {number}\n");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+
+        Console.ReadKey();
+    }
+}
