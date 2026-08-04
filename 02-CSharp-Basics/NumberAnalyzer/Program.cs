0c3ea24cd47239e4aef2e293597a434d8036772e
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-08-04 12:01:20 +0530

    made mini project using loops

---
+++ 02-CSharp-Basics/NumberAnalyzer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine()!);
+
+        int sum = 0;
+
+        Console.WriteLine("\nNumbers:");
+        for (int i = 1; i <= number; i++)
+        {
+            Console.Write(i + " ");
+            sum += i;
+        }
+
+        Console.WriteLine("\n\nEven Numbers:");
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.WriteLine("\n\nOdd Numbers:");
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.WriteLine($"\n\nSum = {sum}");
+
+    }
+}
