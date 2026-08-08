64e3aff30ee8872260da674c9aadfc1192fdf8b1
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-08-08 13:38:38 +0530

    added basic student management project

---
+++ oops basics/studentmanagement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Student s = new Student();
+
+
+s.DisplayCollege();
+Student.ShowCollegeName();
+
+class Student
+{
+    string Name = "Shubham";
+    int Age = 20;
+    string Course = "Computer Science";
+
+public static void ShowCollegeName()
+{
+    Console.WriteLine("College: ABC College");
+}
+    public void DisplayCollege()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Course: " + Course);
+        Console.WriteLine("College: ABC College");
+    }
+
+    public void DisplayStudent()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Course: " + Course);
+    }
+}
+
+
